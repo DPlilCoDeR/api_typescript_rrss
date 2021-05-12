3518427a51850b79d48c1068283e89b5515a7e0d
doblepapi <jasonbnick@gmial.com>
2021-05-12 21:24:06 +0200

    Eliminacion de dist del repo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("Hola Puto");
+import express from 'express';
+
+function main(express: any) {
+    const app = express();
+    const PORT = 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Server run in port ${PORT}`)
+    })
+
+}
+
+main(express)
