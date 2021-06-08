e2ede038c736b6f8cf121e876c2cab488484aadb
doblepapi <jasonbnick@gmial.com>
2021-06-08 23:00:04 +0200

    TODO repair connect in postController

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -4,23 +4,24 @@ import { ConnectionDB } from "../ConnectionDB";
 import  Post  from "../interfaces/Ipost.interface";
 
 
-export class PostController {
-    connection = new ConnectionDB();
+export class PostsController {
+    private conneDB = new ConnectionDB()
 
-    async getAllPosts(req: Request, res: Response) : Promise<Response>{
-        const posts = await this.connection.connection.query('SELECT * FROM posts')
-        return res.json(posts)
+
+    async getAllPosts(req: Request, res: Response) {
+        const posts = await this.conneDB.connection.query('SELECT * FROM posts')
+        return res.json(posts[0])
     }
 
     async getPost(req: Request, res: Response){
         const idPost = req.params.postId;
-        const post = await this.connection.connection.query('SELECT * FROM posts WHERE id = ?', [idPost])
+        const post = await this.conneDB.connection.query('SELECT * FROM posts WHERE id = ?', [idPost])
         return res.json(post[0])
     }
 
     async createPost(req: Request, res: Response) : Promise<Response>{
         const newPost: Post = req.body;
-        await this.connection.connection.query('INSERT INTO posts SET ?', [newPost])
+        await this.conneDB.connection.query('INSERT INTO posts SET ?', [newPost])
         return res.json({
             message: 'Post created'
         });
@@ -29,15 +30,17 @@ export class PostController {
     async updatePost(req: Request, res: Response) : Promise<Response>{
         const updatedBodyPost = req.body;
         const idPost = req.params.postId
-        await this.connection.connection.query('UPDATE posts set ? WHERE id = ?', [updatedBodyPost, idPost])
+        await this.conneDB.connection.query('UPDATE posts set ? WHERE id = ?', [updatedBodyPost, idPost])
         return res.json({
-            response: 'Post updated'
+            message: `Post ${idPost} updated`
         })
     }
 
     async deletePost(req: Request, res: Response) : Promise<Response>{
+        const idPost = req.params.postId;
+        await this.conneDB.connection.query('DELETE FROM posts WHERE id = ?', [idPost])
         return res.json({
-            message: 'Post deleted'
+            message: `Post ${idPost} deleted`
         })
     }
 }
