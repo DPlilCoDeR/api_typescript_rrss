9f99a6bf8dfb3a641bf117d12dbc86ed3980ab25
doblepapi <jasonbnick@gmial.com>
2021-06-08 19:08:17 +0200

    deleted connections duplicated

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -5,44 +5,39 @@ import  Post  from "../interfaces/Ipost.interface";
 
 
 export class PostController {
-
+    connection = new ConnectionDB();
 
     async getAllPosts(req: Request, res: Response) : Promise<Response>{
-        const connection = new ConnectionDB();
-        const posts = await connection.connection.query('SELECT * FROM posts')
+        const posts = await this.connection.connection.query('SELECT * FROM posts')
         return res.json(posts)
     }
 
     async getPost(req: Request, res: Response){
-
-        const connection = new ConnectionDB();
         const idPost = req.params.postId;
-        const post = await connection.connection.query('SELECT * FROM posts WHERE id = ?', [idPost])
+        const post = await this.connection.connection.query('SELECT * FROM posts WHERE id = ?', [idPost])
         return res.json(post[0])
     }
 
     async createPost(req: Request, res: Response) : Promise<Response>{
-        const connection = new ConnectionDB();
         const newPost: Post = req.body;
-        await connection.connection.query('INSERT INTO posts SET ?', [newPost])
+        await this.connection.connection.query('INSERT INTO posts SET ?', [newPost])
         return res.json({
             message: 'Post created'
         });
     }
 
     async updatePost(req: Request, res: Response) : Promise<Response>{
-        const connection = new ConnectionDB();
         const updatedBodyPost = req.body;
         const idPost = req.params.postId
-        await connection.connection.query('UPDATE posts set ? WHERE id = ?', [updatedBodyPost, idPost])
+        await this.connection.connection.query('UPDATE posts set ? WHERE id = ?', [updatedBodyPost, idPost])
         return res.json({
-            response: 'post Updated'
+            response: 'Post updated'
         })
     }
 
     async deletePost(req: Request, res: Response) : Promise<Response>{
         return res.json({
-            message: 'delete works'
+            message: 'Post deleted'
         })
     }
 }
